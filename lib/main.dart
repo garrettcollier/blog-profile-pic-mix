50e617be113de901a728919e66881f64aa6fe8bb
Connor Escajeda <escajedacj@hendrix.edu>
2022-09-06 13:02:15 -0500

    Tab on task bar leads to new to-do pages

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,14 @@ class ToDoList extends StatefulWidget {
   State createState() => _ToDoListState();
 }
 
+class SecondPage extends StatefulWidget {
+  const SecondPage({key, required this.title}) : super(key: key);
+  @override
+  State createState() => _SecondPageState();
+
+  final String title;
+}
+
 class _ToDoListState extends State<ToDoList> {
   // Dialog with text from https://www.appsdeveloperblog.com/alert-dialog-with-a-text-field-in-flutter/
   final TextEditingController _inputController = TextEditingController();
@@ -133,6 +141,172 @@ class _ToDoListState extends State<ToDoList> {
                 ),
               ),
               ListTile(
+                  title: Text("First"),
+                  onTap: () {
+                    Navigator.pop(context);
+                  }),
+              ListTile(
+                title: Text("Second"),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const SecondPage(title: 'SecondPage');
+                  }));
+                },
+              ),
+              Spacer(
+                flex: 10,
+              ),
+              Align(
+                  alignment: Alignment.bottomRight,
+                  child: FloatingActionButton(
+                      child: const Icon(Icons.add),
+                      onPressed: () {
+                        _displayTextInputDialog(context);
+                      }))
+            ],
+          ),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          children: items.map((item) {
+            return ToDoListItem(
+              item: item,
+              completed: _itemSet.contains(item),
+              onListChanged: _handleListChanged,
+              onDeleteItem: _handleDeleteItem,
+            );
+          }).toList(),
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () {
+              _displayTextInputDialog(context);
+            }));
+  }
+}
+
+class _SecondPageState extends State<SecondPage> {
+  // Dialog with text from https://www.appsdeveloperblog.com/alert-dialog-with-a-text-field-in-flutter/
+  final TextEditingController _inputController = TextEditingController();
+  final ButtonStyle yesStyle = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(fontSize: 20), primary: Colors.green);
+  final ButtonStyle noStyle = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(fontSize: 20), primary: Colors.red);
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    print("Loading Dialog");
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Item To Add'),
+            content: TextField(
+              onChanged: (value) {
+                setState(() {
+                  valueText = value;
+                });
+              },
+              controller: _inputController,
+              decoration:
+                  const InputDecoration(hintText: "type something here"),
+            ),
+            actions: <Widget>[
+              ElevatedButton(
+                key: const Key("OKButton"),
+                style: yesStyle,
+                child: const Text('OK'),
+                onPressed: () {
+                  setState(() {
+                    _handleNewItem(valueText);
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+
+              // https://stackoverflow.com/questions/52468987/how-to-turn-disabled-button-into-enabled-button-depending-on-conditions
+              ValueListenableBuilder<TextEditingValue>(
+                valueListenable: _inputController,
+                builder: (context, value, child) {
+                  return ElevatedButton(
+                    key: const Key("CancelButton"),
+                    style: noStyle,
+                    onPressed: value.text.isNotEmpty
+                        ? () {
+                            setState(() {
+                              Navigator.pop(context);
+                            });
+                          }
+                        : null,
+                    child: const Text('Cancel'),
+                  );
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  String valueText = "";
+
+  final List<Item> items = [const Item(name: "add more todos")];
+
+  final _itemSet = <Item>{};
+
+  void _handleListChanged(Item item, bool completed) {
+    setState(() {
+      // When a user changes what's in the list, you need
+      // to change _itemSet inside a setState call to
+      // trigger a rebuild.
+      // The framework then calls build, below,
+      // which updates the visual appearance of the app.
+
+      items.remove(item);
+      if (!completed) {
+        print("Completing");
+        _itemSet.add(item);
+        items.add(item);
+      } else {
+        print("Making Undone");
+        _itemSet.remove(item);
+        items.insert(0, item);
+      }
+    });
+  }
+
+  void _handleDeleteItem(Item item) {
+    setState(() {
+      print("Deleting item");
+      items.remove(item);
+    });
+  }
+
+  void _handleNewItem(String itemText) {
+    setState(() {
+      print("Adding new item");
+      Item item = Item(name: itemText);
+      items.insert(0, item);
+      _inputController.clear();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('School Work'),
+        ),
+        // drawer code from https://rushabhshah065.medium.com/flutter-navigation-drawer-tab-layout-e74074c249ce
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(color: Colors.green),
+                child: Text(
+                  "Testing",
+                  textAlign: TextAlign.justify,
+                  textScaleFactor: 2.0,
+                ),
+              ),
+              ListTile(
                 title: Text("First"),
                 onTap: () {
                   Navigator.pop(context);
@@ -143,7 +317,17 @@ class _ToDoListState extends State<ToDoList> {
                 onTap: () {
                   Navigator.pop(context);
                 },
-              )
+              ),
+              Spacer(
+                flex: 10,
+              ),
+              Align(
+                  alignment: Alignment.bottomRight,
+                  child: FloatingActionButton(
+                      child: const Icon(Icons.add),
+                      onPressed: () {
+                        _displayTextInputDialog(context);
+                      }))
             ],
           ),
         ),
