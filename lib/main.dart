c5ce3ae1dd736029016557ba3da80975dc8a5e9c
Forks <garrettcollierbusiness@gmail.com>
2022-10-25 12:32:11 -0500

    Comment out other class and update call to TrackList

--- lib/main.dart
+++ lib/main.dart
@@ -131,7 +131,7 @@ class _TrackListState extends State<TrackList> {
                 title: Text("Distance"),
                 onTap: () {
                   Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return const SecondPage(title: 'Distance');
+                    return const TrackList(title: 'Distance');
                   }));
                 },
               ),
@@ -155,150 +155,152 @@ class _TrackListState extends State<TrackList> {
   }
 }
 
-class SecondPage extends StatefulWidget {
-  const SecondPage({key, required this.title}) : super(key: key);
-  @override
-  State createState() => _SecondPageState();
-  final String title;
-}
+// UNNEEDED CLASS (BOTH PAGES CALL TRACKLIST CLASS)
 
-class _SecondPageState extends State<SecondPage> {
-  var eventController = TextEditingController();
-  var markController = TextEditingController();
-  var yearController = TextEditingController();
-  var meetController = TextEditingController();
-  //Form code comes from https://stackoverflow.com/questions/54480641/flutter-how-to-create-forms-in-popup
-  // ignore: non_constant_identifier_names
-  Future<void> _EventInfoPopupForm(BuildContext context) async {
-    return showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Add Event'),
-          content: SingleChildScrollView(
-            child: Container(
-              height: 200,
-              width: 200,
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: eventController,
-                    decoration: InputDecoration(hintText: 'Event'),
-                  ),
-                  TextFormField(
-                    controller: markController,
-                    decoration: InputDecoration(hintText: 'Mark'),
-                  ),
-                  TextFormField(
-                    controller: yearController,
-                    decoration: InputDecoration(hintText: 'Year'),
-                  ),
-                  TextFormField(
-                    controller: meetController,
-                    decoration: InputDecoration(hintText: 'Meet'),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  _handleTrackItem(eventController.text, markController.text,
-                      yearController.text, meetController.text);
-                  Navigator.pop(context);
-                });
-              },
-              child: Text('Send'),
-            ),
-          ],
-        );
-      },
-    );
-  }
+// class SecondPage extends StatefulWidget {
+//   const SecondPage({key, required this.title}) : super(key: key);
+//   @override
+//   State createState() => _SecondPageState();
+//   final String title;
+// }
 
-  String valueText = "";
+// class _SecondPageState extends State<SecondPage> {
+//   var eventController = TextEditingController();
+//   var markController = TextEditingController();
+//   var yearController = TextEditingController();
+//   var meetController = TextEditingController();
+//   //Form code comes from https://stackoverflow.com/questions/54480641/flutter-how-to-create-forms-in-popup
+//   // ignore: non_constant_identifier_names
+//   Future<void> _EventInfoPopupForm(BuildContext context) async {
+//     return showDialog(
+//       context: context,
+//       builder: (context) {
+//         return AlertDialog(
+//           title: Text('Add Event'),
+//           content: SingleChildScrollView(
+//             child: Container(
+//               height: 200,
+//               width: 200,
+//               child: Column(
+//                 children: [
+//                   TextFormField(
+//                     controller: eventController,
+//                     decoration: InputDecoration(hintText: 'Event'),
+//                   ),
+//                   TextFormField(
+//                     controller: markController,
+//                     decoration: InputDecoration(hintText: 'Mark'),
+//                   ),
+//                   TextFormField(
+//                     controller: yearController,
+//                     decoration: InputDecoration(hintText: 'Year'),
+//                   ),
+//                   TextFormField(
+//                     controller: meetController,
+//                     decoration: InputDecoration(hintText: 'Meet'),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//           actions: <Widget>[
+//             TextButton(
+//               onPressed: () => Navigator.pop(context),
+//               child: Text('Cancel'),
+//             ),
+//             TextButton(
+//               onPressed: () {
+//                 setState(() {
+//                   _handleTrackItem(eventController.text, markController.text,
+//                       yearController.text, meetController.text);
+//                   Navigator.pop(context);
+//                 });
+//               },
+//               child: Text('Send'),
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
 
-  //final List<Item> items = [const Item(name: "Add homework")];
+//   String valueText = "";
 
-  final _itemSet = <Item>{};
+//   //final List<Item> items = [const Item(name: "Add homework")];
 
-  void _handleTrackItem(event, mark, year, meet) {
-    setState(() {
-      Event _event = Event(event: event, mark: mark, year: year, meet: meet);
-      sprints.insert(0, _event);
-      eventController.clear();
-      markController.clear();
-      yearController.clear();
-      meetController.clear();
-    });
-  }
+//   final _itemSet = <Item>{};
 
-  void _handleEventEdit(Event event) {
-    setState(() {
-      sprints.remove(event);
-      _EventInfoPopupForm(context);
-    });
-  }
+//   void _handleTrackItem(event, mark, year, meet) {
+//     setState(() {
+//       Event _event = Event(event: event, mark: mark, year: year, meet: meet);
+//       sprints.insert(0, _event);
+//       eventController.clear();
+//       markController.clear();
+//       yearController.clear();
+//       meetController.clear();
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Distance Personal Records'),
-        ),
-        // drawer code from https://rushabhshah065.medium.com/flutter-navigation-drawer-tab-layout-e74074c249ce
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                decoration: BoxDecoration(color: Colors.green),
-                child: Text(
-                  "Type of Event",
-                  textAlign: TextAlign.justify,
-                  textScaleFactor: 2.0,
-                ),
-              ),
-              ListTile(
-                  title: Text("Sprints"),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return const TrackList(title: 'Sprints');
-                    }));
-                  }),
-              ListTile(
-                title: Text("Distance"),
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return const SecondPage(title: 'Distance');
-                  }));
-                },
-              ),
-            ],
-          ),
-        ),
-        body: ListView(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          children: distance.map((item) {
-            return EventItem(
-              event: item,
-              eventEdit: _handleEventEdit,
-            );
-          }).toList(),
-        ),
-        floatingActionButton: FloatingActionButton(
-            child: const Icon(Icons.add),
-            onPressed: () {
-              _EventInfoPopupForm(context);
-            }));
-  }
-}
+//   void _handleEventEdit(Event event) {
+//     setState(() {
+//       sprints.remove(event);
+//       _EventInfoPopupForm(context);
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Distance Personal Records'),
+//         ),
+//         // drawer code from https://rushabhshah065.medium.com/flutter-navigation-drawer-tab-layout-e74074c249ce
+//         drawer: Drawer(
+//           child: ListView(
+//             children: [
+//               DrawerHeader(
+//                 decoration: BoxDecoration(color: Colors.green),
+//                 child: Text(
+//                   "Type of Event",
+//                   textAlign: TextAlign.justify,
+//                   textScaleFactor: 2.0,
+//                 ),
+//               ),
+//               ListTile(
+//                   title: Text("Sprints"),
+//                   onTap: () {
+//                     Navigator.push(context,
+//                         MaterialPageRoute(builder: (context) {
+//                       return const TrackList(title: 'Sprints');
+//                     }));
+//                   }),
+//               ListTile(
+//                 title: Text("Distance"),
+//                 onTap: () {
+//                   Navigator.push(context, MaterialPageRoute(builder: (context) {
+//                     return const SecondPage(title: 'Distance');
+//                   }));
+//                 },
+//               ),
+//             ],
+//           ),
+//         ),
+//         body: ListView(
+//           padding: const EdgeInsets.symmetric(vertical: 8.0),
+//           children: distance.map((item) {
+//             return EventItem(
+//               event: item,
+//               eventEdit: _handleEventEdit,
+//             );
+//           }).toList(),
+//         ),
+//         floatingActionButton: FloatingActionButton(
+//             child: const Icon(Icons.add),
+//             onPressed: () {
+//               _EventInfoPopupForm(context);
+//             }));
+//   }
+// }
 
 void main() {
   runApp(const MaterialApp(
